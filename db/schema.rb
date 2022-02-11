a50a6ba0f3f4608031f614b8c5acd0324bfc590b
Stephen Yeargin <stephen.yeargin@vumc.org>
2022-02-11 15:33:31 -0600

    Don't set collation

--- db/schema.rb
+++ db/schema.rb
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 2022_02_11_203954) do
     t.index ["region_id"], name: "index_chapters_on_region_id"
   end
 
-  create_table "districts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "districts", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.string "short_name"
     t.integer "position", default: 0
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 2022_02_11_203954) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "regions", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "regions", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.string "short_name"
     t.integer "position", default: 0
@@ -45,7 +45,7 @@ ActiveRecord::Schema.define(version: 2022_02_11_203954) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
