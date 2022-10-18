46764d9b373ec951909a8c2406c10e0dae62d41d
Stephen Yeargin <stephen.yeargin@gmail.com>
2022-10-17 21:34:45 -0500

    Fix schema x2

--- db/schema.rb
+++ db/schema.rb
@@ -44,7 +44,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_11_203954) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", charset: "utf8mb3", force: :cascade do |t|
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
