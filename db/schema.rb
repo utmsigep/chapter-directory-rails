751e69edc1fc3e2f3d64ae33a6898f9d4eb72f09
Stephen Yeargin <stephen.yeargin@gmail.com>
2023-04-08 14:11:00 -0500

    Fix users table schema

--- db/schema.rb
+++ db/schema.rb
@@ -47,7 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_08_035832) do
     t.string "staff_url"
   end
 
-  create_table "users", charset: "utf8mb3", collation: "utf8mb3_general_ci", force: :cascade do |t|
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
