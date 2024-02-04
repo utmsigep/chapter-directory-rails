fffffa919fc46c1014fa2675d59b858fbd8f5d80
Stephen Yeargin <stephen.yeargin@gmail.com>
2024-02-04 10:22:29 -0600

    Reset

--- app/javascript/controllers/admin_controller.js
+++ app/javascript/controllers/admin_controller.js
@@ -1,28 +1,11 @@
 import { Controller } from '@hotwired/stimulus';
-import Chartkick from 'chartkick';
 
-require('chartkick/chart.js');
 require('datatables.net');
 require('datatables.net-bs5');
 
 export default class extends Controller {
-  static targets = [];
-
-  static values = {
-    type: String,
-    data: Array,
-    options: Object,
-  };
-
-  chart = null;
-
   connect() {
     this.table = document.getElementsByClassName('table-admin').DataTable();
-    this.chart = new Chartkick[this.typeValue](
-      this.element.id,
-      this.dataValue,
-      this.optionsValue,
-    );
   }
 
   disconnect() {
