61b174e5a075fba1507e867bbcce405c3d4157d1
Stephen Yeargin <stephen@yearg.in>
2024-09-07 19:19:44 -0500

    Normalize institution_name

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -5,14 +5,19 @@ class Chapter < ApplicationRecord
   has_many :manpower_surveys
   validates_presence_of :name
 
+  before_save :normalize_field
+
   scope :active, -> { where(status: 1) }
   scope :inactive, -> { where(status: 0) }
 
   include GenerateCsv
 
-  def institution_name=(val)
-    self[:institution_name] = val.gsub(' Of ', ' of ')
-    self[:institution_name] = val.gsub(' At ', ' at ')
+  def normalize_field
+    return unless institution_name.present?
+
+    institution_name.gsub!(' Of ', ' of ')
+    institution_name.gsub!(' At ', ' at ')
+    institution_name.gsub!(' And ', ' and ')
   end
 
   def self.search(query, include_inactive = false)
